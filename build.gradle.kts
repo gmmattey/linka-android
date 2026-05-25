b88ffc56949fe3c868fe072161365e9a4187d349
gmmattey <gmmattey@users.noreply.github.com>
2026-05-25 00:20:57 -0300

    fix(build): usar id explícito para plugin crashlytics no root build.gradle (#39)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.appdistribution") version "5.1.1" apply false
-    alias(libs.plugins.firebaseCrashlyticsPlugin) apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.ktlint) apply false
