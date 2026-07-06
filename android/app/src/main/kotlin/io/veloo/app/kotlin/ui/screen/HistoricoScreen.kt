a5fa0b0d1f017911550aca35c215187d462c26ed
gmmattey <gmmattey@users.noreply.github.com>
2026-07-06 07:50:12 -0300

    fix(lint): corrigir ordem de import isSystemInDarkTheme (ktlint)

--- android/app/src/main/kotlin/io/veloo/app/kotlin/ui/screen/HistoricoScreen.kt
+++ android/app/src/main/kotlin/io/veloo/app/kotlin/ui/screen/HistoricoScreen.kt
@@ -3,8 +3,8 @@
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
